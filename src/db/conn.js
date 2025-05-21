c55f27b37e4d435edc957a1f9d9551aebe40b5cc
Dhwanivd <dhwanivd024@gmail.com>
2025-05-21 15:40:18 +0530

    DATABASE error correction2

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose');
-main()
-    .then( ()=>{
-        console.log("connection successful");
-    })
-    .catch( (err)=> console.log(err));
 
-async function main(){
-//  await mongoose.connect('mongodb://127.0.0.1:27017/GlucoSage');
-    await mongoose.connect('mongodb+srv://dhwani24:<*24122003dD>@cluster0.zsvhh0y.mongodb.net/');
-
+async function main() {
+  await mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 }
+
+main()
+  .then(() => {
+    console.log("MongoDB connection successful");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection failed:", err);
+  });
